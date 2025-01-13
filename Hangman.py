d0c8a688e273b2dc0673b2bb0f495c17ec965f7d
Shruthika-s <sunkushruthika@gmail.com>
2025-01-13 20:10:30 +0530

    commit

---
+++ Hangman.py
@@ -0,0 +1,317 @@
+import random
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+word_list = [
+    'abruptly',
+    'absurd',
+    'abyss',
+    'affix',
+    'askew',
+    'avenue',
+    'awkward',
+    'axiom',
+    'azure',
+    'bagpipes',
+    'bandwagon',
+    'banjo',
+    'bayou',
+    'beekeeper',
+    'bikini',
+    'blitz',
+    'blizzard',
+    'boggle',
+    'bookworm',
+    'boxcar',
+    'boxful',
+    'buckaroo',
+    'buffalo',
+    'buffoon',
+    'buxom',
+    'buzzard',
+    'buzzing',
+    'buzzwords',
+    'caliph',
+    'cobweb',
+    'cockiness',
+    'croquet',
+    'crypt',
+    'curacao',
+    'cycle',
+    'daiquiri',
+    'dirndl',
+    'disavow',
+    'dizzying',
+    'duplex',
+    'dwarves',
+    'embezzle',
+    'equip',
+    'espionage',
+    'euouae',
+    'exodus',
+    'faking',
+    'fishhook',
+    'fixable',
+    'fjord',
+    'flapjack',
+    'flopping',
+    'fluffiness',
+    'flyby',
+    'foxglove',
+    'frazzled',
+    'frizzled',
+    'fuchsia',
+    'funny',
+    'gabby',
+    'galaxy',
+    'galvanize',
+    'gazebo',
+    'giaour',
+    'gizmo',
+    'glowworm',
+    'glyph',
+    'gnarly',
+    'gnostic',
+    'gossip',
+    'grogginess',
+    'haiku',
+    'haphazard',
+    'hyphen',
+    'iatrogenic',
+    'icebox',
+    'injury',
+    'ivory',
+    'ivy',
+    'jackpot',
+    'jaundice',
+    'jawbreaker',
+    'jaywalk',
+    'jazziest',
+    'jazzy',
+    'jelly',
+    'jigsaw',
+    'jinx',
+    'jiujitsu',
+    'jockey',
+    'jogging',
+    'joking',
+    'jovial',
+    'joyful',
+    'juicy',
+    'jukebox',
+    'jumbo',
+    'kayak',
+    'kazoo',
+    'keyhole',
+    'khaki',
+    'kilobyte',
+    'kiosk',
+    'kitsch',
+    'kiwifruit',
+    'klutz',
+    'knapsack',
+    'larynx',
+    'lengths',
+    'lucky',
+    'luxury',
+    'lymph',
+    'marquis',
+    'matrix',
+    'megahertz',
+    'microwave',
+    'mnemonic',
+    'mystify',
+    'naphtha',
+    'nightclub',
+    'nowadays',
+    'numbskull',
+    'nymph',
+    'onyx',
+    'ovary',
+    'oxidize',
+    'oxygen',
+    'pajama',
+    'peekaboo',
+    'phlegm',
+    'pixel',
+    'pizazz',
+    'pneumonia',
+    'polka',
+    'pshaw',
+    'psyche',
+    'puppy',
+    'puzzling',
+    'quartz',
+    'queue',
+    'quips',
+    'quixotic',
+    'quiz',
+    'quizzes',
+    'quorum',
+    'razzmatazz',
+    'rhubarb',
+    'rhythm',
+    'rickshaw',
+    'schnapps',
+    'scratch',
+    'shiv',
+    'snazzy',
+    'sphinx',
+    'spritz',
+    'squawk',
+    'staff',
+    'strength',
+    'strengths',
+    'stretch',
+    'stronghold',
+    'stymied',
+    'subway',
+    'swivel',
+    'syndrome',
+    'thriftless',
+    'thumbscrew',
+    'topaz',
+    'transcript',
+    'transgress',
+    'transplant',
+    'triphthong',
+    'twelfth',
+    'twelfths',
+    'unknown',
+    'unworthy',
+    'unzip',
+    'uptown',
+    'vaporize',
+    'vixen',
+    'vodka',
+    'voodoo',
+    'vortex',
+    'voyeurism',
+    'walkway',
+    'waltz',
+    'wave',
+    'wavy',
+    'waxy',
+    'wellspring',
+    'wheezy',
+    'whiskey',
+    'whizzing',
+    'whomever',
+    'wimpy',
+    'witchcraft',
+    'wizard',
+    'woozy',
+    'wristwatch',
+    'wyvern',
+    'xylophone',
+    'yachtsman',
+    'yippee',
+    'yoked',
+    'youthful',
+    'yummy',
+    'zephyr',
+    'zigzag',
+    'zigzagging',
+    'zilch',
+    'zipper',
+    'zodiac',
+    'zombie',
+]
+
+stages = [
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
+
+print(logo)
+word = random.choice(word_list)
+display = []
+for letter in word:
+    display += "_"
+print(f"{' '.join(display)}")
+lives = 6
+print(stages[lives])
+
+
+while "_" in display and lives > 0:  
+    guess = input("Guess a letter: ").lower()
+    f = 0
+    for i in range(len(word)):
+        if word[i] == guess:
+            display[i] = guess
+            f = 1
+    if f == 1:
+        print("You guessed a letter that is there in the word!")
+    if f == 0:
+        print(
+            "You guessed a letter that is not there in the word. You lose a life!"
+        )
+        lives -= 1
+    print(f"{' '.join(display)}")
+    print(stages[lives])
+if "_" not in display:
+    print("You guesses the word. You win!")
+if lives == 0:
+    print("You ran out of lives. You lose!")
+    print("The word is",word)
